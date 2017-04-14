b6f83e5fc825b3cb713438466122f433847ea410
Mello244688 <Scott.Mello24@gmail.com>
2017-04-14 18:22:24 -0400

    heroku not loading resources

--- userInterface.rkt
+++ userInterface.rkt
@@ -5,6 +5,9 @@
 
 (define root (current-directory))
 
+(define (asset-url path)
+  (string-append "/assets/" path))
+
 (define port (if (getenv "PORT")
                  (string->number (getenv "PORT"))
                  8080))
@@ -16,10 +19,8 @@
                        (href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css")
                        (type "text/css")))
                 (link ((rel "stylesheet")
-                       (href "chat_robot.css")
-                       (type "text/css")))
-                (script ((src "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js")))
-                (script ((src "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"))))
+                       (href ,( asset-url "chat_robot.css"))
+                       (type "text/css"))))
           (body (div ((class "jumbotron text-center"))
                      (h1 "Chat Robot")
                      (p "Talk to the chat robot"))
@@ -37,14 +38,14 @@
                                            (button ((class "btn btn-primary")
                                                     (id "submitButton")
                                                     (type "submit")) "Send"))))))
-                (script ((src "chatRobot.js")))))))
+                (script ((src "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js")))
+                (script ((src "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js")))
+                (script ((src ,(asset-url "/js/chatRobot.js"))))))))
+
 
 (serve/servlet chat-robot-form
                  #:servlet-path "/"
                  #:port port
                  #:listen-ip #f
                  #:command-line? #t
-                 #:extra-files-paths
-                 (list
-                  (build-path root "css")
-                  (build-path root "js")))
+                 #:extra-files-paths (list "public"))
